82bb81250b33137fbfc2a4266fff4154521521d9
Bachir <49397432+bachir911@users.noreply.github.com>
2019-04-10 12:36:30 +0000

    Add files via upload
    correction TP calcul de la moyenne de deux valeurs

---
+++ test.py
@@ -0,0 +1,12 @@
+calcul de moyenne de deux valeurs
+
+x = input("saisir premiere valeur")
+
+y = input("saisir deuxieme valeur")
+
+somme = x + y
+
+moyenne = int((somme)/2)
+
+print("somme est de")
+print("moyenne \n")
